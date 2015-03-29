ef86524a054658f4a979768a93ce9640f5252e3a
Victor Grigoriu <vgrigoriu@gmail.com>
2015-03-29 20:14:02 +0300

    Selfhosted owin serving static files

--- src/Comments/Program.cs
+++ src/Comments/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+using Owin;
 
 namespace Comments
 {
@@ -10,6 +8,18 @@ namespace Comments
     {
         static void Main(string[] args)
         {
+            using (WebApp.Start<Startup>("http://localhost:12345"))
+            {
+                Console.ReadLine();
+            }
         }
+    }
+}
+
+public class Startup
+{
+    public void Configuration(IAppBuilder app)
+    {
+        app.UseFileServer(true);
     }
 }
