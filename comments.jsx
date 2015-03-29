aea92ea3cbab34615ab25a9ea20548bd490b14b6
Victor Grigoriu <vgrigoriu@gmail.com>
2015-03-29 19:32:39 +0300

    Comment list is data-driven

--- comments.jsx
+++ comments.jsx
@@ -1,5 +1,10 @@
 var converter = new Showdown.converter();
 
+var data = [
+    {key: 1, author: "Baba Cloanța", text: "Un comentariu obraznic"},
+    {key: 1, author: "Făt Frumos", text: "Ba *muma* Dvs."}
+];
+
 var Comment = React.createClass({
     render: function () {
         var rawMarkup = converter.makeHtml(this.props.children.toString());
@@ -17,10 +22,17 @@ var Comment = React.createClass({
 
 var CommentList = React.createClass({
     render: function () {
+        var commentNodes = this.props.data.map(function (comment) {
+            return (
+                <Comment key={comment.key} author={comment.author}>
+                    {comment.text}
+                </Comment>
+            );
+        });
+
         return (
             <div className="commentList">
-                <Comment author="Baba Cloanța">Un comentariu</Comment>
-                <Comment author="Făt Frumos">Ba *mama* Dvs.</Comment>
+                {commentNodes}
             </div>
         );
     }
@@ -41,11 +53,11 @@ var CommentBox = React.createClass({
         return (
             <div className="commentBox">
                 <h1>Comentarii</h1>
-                <CommentList />
+                <CommentList data={this.props.data} />
                 <CommentForm />
             </div>
         );
     }
 });
 
-React.render(<CommentBox />, document.getElementById('content'));
+React.render(<CommentBox data={data} />, document.getElementById('content'));
