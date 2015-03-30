8d0e557b7d6054b9f9b83abad4e20b168064b6ef
Victor Grigoriu <vgrigoriu@gmail.com>
2015-03-30 21:12:27 +0300

    Move startup class to its own file

--- src/Comments/Program.cs
+++ src/Comments/Program.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Web.Http;
-using Microsoft.Owin.FileSystems;
 using Microsoft.Owin.Hosting;
-using Microsoft.Owin.StaticFiles;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
-using Owin;
 
 namespace Comments
 {
@@ -18,51 +12,5 @@ namespace Comments
                 Console.ReadLine();
             }
         }
-    }
-}
-
-public class Startup
-{
-    public void Configuration(IAppBuilder app)
-    {
-        ConfigureFileServer(app);
-        ConfigureWebApi(app);
-    }
-
-    private void ConfigureFileServer(IAppBuilder app)
-    {
-        var options = new FileServerOptions()
-        {
-            FileSystem = new PhysicalFileSystem("../../../public/"),
-        };
-
-        app.UseFileServer(options);
-    }
-
-    private void ConfigureWebApi(IAppBuilder app)
-    {
-        var config = new HttpConfiguration();
-        ConfigureWebApiRoutes(config);
-        ConfigureJsonFormatter(config);
-
-        app.UseWebApi(config);
-    }
-
-    private void ConfigureWebApiRoutes(HttpConfiguration config)
-    {
-        config.Routes.MapHttpRoute(
-            name: "Default",
-            routeTemplate: "comments.json",
-            defaults: new { controller = "Comments" });
-    }
-
-    private void ConfigureJsonFormatter(HttpConfiguration config)
-    {
-        var formatters = config.Formatters;
-        var jsonFormatter = formatters.JsonFormatter;
-        var settings = jsonFormatter.SerializerSettings;
-
-        settings.Formatting = Formatting.Indented;
-        settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
     }
 }
