f1d2bbc520f5b26a1dbbc4382942d4e51b57110e
Victor Grigoriu <vgrigoriu@gmail.com>
2015-03-30 20:57:40 +0300

    keep a collection of comments

--- src/Comments/Controllers/CommentsController.cs
+++ src/Comments/Controllers/CommentsController.cs
@@ -1,17 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Web.Http;
 
 namespace Comments.Controllers
 {
     public class CommentsController : ApiController
     {
+        private static ICollection<Comment> comments = new List<Comment>
+        {
+            new Comment("Castor", "un elefent se legăna"),
+            new Comment("Polux", "podul de piatră s-a dărîmat"),
+            new Comment("Romulus", "lorem ipsum dolorosa")
+        };
+
         public IHttpActionResult Get()
         {
-            return Ok(new[]
-            {
-                new {id = 1, author = "vasile",  text = "vasile dixit la ora " + DateTimeOffset.Now},
-                new {id = 2, author = "horia",  text = "horia vorbește _apăsat_"}
-            });
+            return Ok(comments);
+        }
+    }
+
+    public class Comment
+    {
+        private static int currentId = 0;
+
+        public int Id { get; } = currentId++;
+
+        public string Author { get; }
+
+        public string Text { get; }
+
+        public Comment(string author, string text)
+        {
+            Author = author;
+            Text = text;
         }
     }
 }
